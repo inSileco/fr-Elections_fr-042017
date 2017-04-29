18bf04ef1aa9c0df6bd706b96b178d0d4c09a132
Steve Vissault <s.vissault@yahoo.fr>
2017-04-29 10:06:15 -0400

    add satwrld_adm <- readOGR(dsn="data/countries", layer="countries")

--- map_elections.R
+++ map_elections.R
@@ -2,6 +2,7 @@ library(htmlwidgets)
 library(htmltools)
 library(leaflet)
 library(rgdal)
+library(rgeos)
 
 ## This is what I used to make the dataset ready
 # library(rgdal)
@@ -13,7 +14,6 @@ library(rgdal)
 # writeOGR(wsimp, dsn="country_geojson", layer="world", driver="GeoJSON")
 
 wrld_adm <- readOGR(dsn="data/countries", layer="countries")
-
 # Candidats
 df_score <- read.csv2("data/Tour_1_Resultats_par_pays_240417.csv",
 encoding="Latin-1")
@@ -22,10 +22,15 @@ ls_labels <- sprintf(
   wrld_adm@data$ISO3) %>% lapply(htmltools::HTML)
 
 # Map
-map_elec <- leaflet(wrld_adm) %>%
+map_elec <- leaflet(wrld_adm_gsimp) %>%
   setView(lng = 5, lat = 10, zoom = 3) %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
   addPolygons(
+    weight = 2,
+    opacity = 1,
+    color = "white",
+    dashArray = "3",
+    fillOpacity = 0.7,
     highlight = highlightOptions(
       weight = 3,
       color = "#666",
@@ -36,6 +41,6 @@ map_elec <- leaflet(wrld_adm) %>%
     labelOptions = labelOptions(
       style = list("font-weight" = "normal", padding = "3px 8px"),
       textsize = "15px", direction = "auto")
-      ) %>% addProviderTiles(providers$Stamen.Toner)
+      ) %>% addProviderTiles(providers$Esri.WorldImagery,options = providerTileOptions(minZoom=10, maxZoom=18))
 
-saveWidget(widget = map_elec, file = "index.html")
+saveWidget(widget = map_elec, file = "./index.html")
